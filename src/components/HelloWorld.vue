8bae8da8ae0a394e7678ab05f5f82e3ca1d80c37
「安忠琪」 <anzhongqi@marknum.com>
2023-03-08 17:59:52 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@ import { onMounted, ref } from 'vue'
 import * as echarts from 'echarts'
 import 'echarts-gl'
 import jsonData from './map.json'
-import detailTexture from '../assets/img_00048.png'
+import detailTexture from '../assets/ditu.png'
 onMounted(() => {
   const dom = document.getElementById('maper')
   const myChart = echarts.init(dom)
@@ -15,17 +15,26 @@ onMounted(() => {
       height: Math.max(Math.sqrt(feature.properties.height), 0.1),
     }
   })
+  console.log(regions)
   myChart.setOption({
     series: [
       {
         type: 'map3D',
         map: '德兴市',
         shading: 'realistic', // 真实感渲染，配合 light.ambientCubemap 和 postEffect 使用可以让展示的画面效果和质感有质的提升
+        // boxDepth: 180, //地图倾斜度
+        regionHeight: 15, //地图高度
         // 真实感材质相关的配置项
         realisticMaterial: {
           roughness: 1, //表示材质的粗糙度，0为完全光滑，1完全粗糙，中间的值则是介于这两者之间。
-          textureTiling: 1, //材质细节纹理的平铺。默认为1，也就是拉伸填满。大于 1 的时候，数字表示纹理平铺重复的次数。
+          textureTiling: [1, 1], //材质细节纹理的平铺。默认为1，也就是拉伸填满。大于 1 的时候，数字表示纹理平铺重复的次数。
           detailTexture: detailTexture, //纹理贴图。
+          // normalTexture: detailTexture,
+          textureOffset: 0,
+          roughness: 0, // 粗糙度
+          metalness: 0,
+          roughnessAdjust: 0,
+          metalnessAdjust: 0,
         },
         // 后处理特效的相关配置项，为画面添加高光、景深、环境光遮蔽（SSAO）、调色等效果
         // postEffect: {
@@ -81,17 +90,18 @@ onMounted(() => {
           panMouseButton: 'right', // 平移鼠标按键
           minBeta: -360,
           maxBeta: 360,
+          distance: 150, //地图视角 控制初始大小
         },
         itemStyle: {
-          areaColor: '#666',
+          areaColor: '#fff',
         },
         label: {
-          color: 'white',
+          color: '#fff',
         },
         silent: true,
         instancing: true,
         boxWidth: 200,
-        boxHeight: 1,
+        boxHeight: 100,
         data: regions,
       },
     ],
